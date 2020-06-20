2b285aa42008137601afb3374c992cc5dd128d58
Akira Kurusu <43274844+AkiraKurusuOnGitHub@users.noreply.github.com>
2020-06-20 23:16:36 +0200

    Add files via upload
    New functions added

--- Start-Stop Apache.cmd
+++ Start-Stop Apache.cmd
@@ -5,6 +5,7 @@ if %errorlevel% == 0 (goto running) else (goto not_running)
 
 :running
 echo The 'Apache2.4' service is running.
+echo.
 goto Input
 
 :not_running
@@ -12,20 +13,29 @@ echo The 'Apache2.4' service is not running.
 goto Input
 
 :Input
-set /p INPUT="start - stop - quit: "
+set /p INPUT="start - stop - restart - quit: "
 
 If /I "%INPUT%" == "start" goto start
 If /I "%INPUT%" == "stop" goto stop
+If /I "%INPUT%" == "restart" goto restart
 goto END
 
 :stop
 httpd -k stop
-goto END
+echo.
+goto Input
 
 :start
 httpd -k start
 echo The 'Apache2.4' service is now running.
-goto END
+echo.
+goto Input
+
+:restart
+httpd -k restart
+echo The 'Apache2.4' service has restarted.
+echo.
+goto Input
 
 :END
 echo.
